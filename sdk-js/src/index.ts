d706b446afd42119d12243f7c78a74036789e303
Yusrah Mohammed <mohammedyusi6@gmail.com>
2026-03-25 12:23:42 +0100

    feat/1-2-3-4-contract-harness-validation-sdk-bootstrap

--- sdk-js/src/index.ts
+++ sdk-js/src/index.ts
@@ -1,2 +1,45 @@
-export * from './client.js';
-export * from './types.js';
+/**
+ * @stellarroute/sdk-js
+ *
+ * TypeScript SDK for the StellarRoute DEX aggregation API.
+ *
+ * @example
+ * ```ts
+ * import { StellarRouteClient, isStellarRouteApiError } from '@stellarroute/sdk-js';
+ *
+ * const client = new StellarRouteClient({ baseUrl: 'https://api.stellarroute.io' });
+ *
+ * try {
+ *   const quote = await client.getQuote('native', 'USDC', 100);
+ *   console.log(quote.price);
+ * } catch (err) {
+ *   if (isStellarRouteApiError(err) && err.isNotFound()) {
+ *     console.log('no route found for this pair');
+ *   }
+ * }
+ * ```
+ *
+ * @packageDocumentation
+ */
+
+export {
+  StellarRouteClient,
+  StellarRouteApiError,
+  isStellarRouteApiError,
+} from './client.js';
+
+export type { StellarRouteClientOptions } from './client.js';
+
+export type {
+  ApiError,
+  ApiErrorCode,
+  Asset,
+  HealthStatus,
+  Orderbook,
+  OrderbookEntry,
+  PairsResponse,
+  PathStep,
+  PriceQuote,
+  QuoteType,
+  TradingPair,
+} from './types.js';
